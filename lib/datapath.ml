6f5b9d352abf7498487647537966bec9515088fc
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-07-22 00:17:15 -0400

    Non-width-compliant inputs

--- lib/datapath.ml
+++ lib/datapath.ml
@@ -2,13 +2,15 @@ open Hardcaml
 open Hardcaml.Signal
 
 module I = struct
-  type 'a t = { clock : 'a; suffix: 'a } [@@deriving sexp_of, hardcaml]
+  type 'a t = { clock : 'a; suffix : 'a } [@@deriving sexp_of, hardcaml]
 end
 
 module O = struct
   type 'a t = { pc : 'a [@bits 5] } [@@deriving sexp_of, hardcaml]
 end
 
-let create (scope : Scope.t) (i : _ I.t) = 
-  let instruction_fetch = Instruction_fetch.hierarchical scope {clock = i.clock} in
-  { O.pc = instruction_fetch.instruction.:[3, 0] @: i.suffix }
+let create (scope : Scope.t) (i : _ I.t) =
+  let instruction_fetch =
+    Instruction_fetch.hierarchical scope { clock = of_string "5'b11111" }
+  in
+  { O.pc = instruction_fetch.instruction.:[(3, 0)] @: i.clock }
