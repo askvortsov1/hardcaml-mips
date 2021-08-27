55b69ad5f6f3795475f5241e24a6023daf67ee0a
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-08-27 15:12:51 -0400

    Restore working interactive.ml
    This was probably changed by accident.

--- interactive.ml
+++ interactive.ml
@@ -3,10 +3,9 @@ open Hardcaml_waveterm
 open Mips.Datapath
 module Simulator = Cyclesim.With_interface (I) (O)
 
-
 let testbench n =
-  let scope = Scope.create ~flatten_design:true () in
-  let sim = Simulator.create (circuit_impl Mips.Program.sample scope) in
+  let scope = Scope.create ~auto_label_hierarchical_ports:true ~flatten_design:true () in
+  let sim = Simulator.create ~config:Cyclesim.Config.trace_all (circuit_impl Mips.Program.sample scope) in
   let waves, sim = Waveform.create sim in
 
   for _i = 0 to n do
@@ -16,5 +15,5 @@ let testbench n =
   waves
 
 let () =
-  let waves = testbench 150 in
+  let waves = testbench 15 in
   Hardcaml_waveterm_interactive.run ~wave_width:5 ~signals_width:30 waves
