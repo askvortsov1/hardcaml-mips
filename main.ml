b86c4ceee9dc8ef76b6db9b861be3dde2f6f0be9
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-05-18 22:33:10 -0400

    feat: compile verilog for CPU instead of datapath

--- main.ml
+++ main.ml
@@ -1,13 +1,13 @@
 open Hardcaml
 open Mips
-module MipsCircuit = Circuit.With_interface (Datapath.I) (Datapath.O)
+module MipsCircuit = Circuit.With_interface (Cpu.I) (Cpu.O)
 
 let scope = Scope.create ()
 
 let circuit =
-  let bound_circuit_impl = Datapath.circuit_impl Program.sample scope in
-  MipsCircuit.create_exn ~name:"datapath" bound_circuit_impl
+  let bound_circuit_impl = Cpu.circuit_impl Program.sample scope in
+  MipsCircuit.create_exn ~name:"cpu" bound_circuit_impl
 
-let output_mode = Rtl.Output_mode.To_file("mips_datapath.v")
+let output_mode = Rtl.Output_mode.To_file("mips_cpu.v")
 
 let () = Rtl.output ~output_mode ~database:(Scope.circuit_database scope) Verilog circuit
